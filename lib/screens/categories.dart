9bf949fd6c39f0fa417639fdf25850d00e617e8f
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-05 10:56:22 +0530

    used Navigator push for connecting the pages and called CategoryGridItem

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          meals: filteredMeals,
+          title: category.title,
+        ),
+      ),
+    ); //Navigator.push(context, route);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +39,11 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                })
         ],
       ),
     );
