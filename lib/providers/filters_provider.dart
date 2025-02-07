40ec4a7d6b130f3c744c9d7420e3d3884a3bad54
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-07 16:20:10 +0530

    Add filterMealsProvider to filter meals based on active filters
    - Extend filtersProvider implementation for centralized filter state.
    - Create filterMealsProvider that watches mealsProvider and filtersProvider.
    - Filter meals based on active filter criteria and return the filtered list.

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
 
@@ -23,3 +24,24 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
 final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
         (ref) => FiltersNotifier());
+
+final filterMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeProvider = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeProvider[Filter.glutenFree]! && meal.isGlutenFree) {
+      return false;
+    }
+    if (activeProvider[Filter.lactoseFree]! && meal.isLactoseFree) {
+      return false;
+    }
+    if (activeProvider[Filter.vegetarian]! && meal.isVegetarian) {
+      return false;
+    }
+    if (activeProvider[Filter.vegan]! && meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
